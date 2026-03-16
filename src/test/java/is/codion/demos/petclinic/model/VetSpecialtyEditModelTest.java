49104b8acb7b17d1d5ab5cc7c487dd41ffeb3c49
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-03-16 11:35:06 +0000

    Codion upgraded

--- src/test/java/is/codion/demos/petclinic/model/VetSpecialtyEditModelTest.java
+++ src/test/java/is/codion/demos/petclinic/model/VetSpecialtyEditModelTest.java
@@ -53,7 +53,7 @@ public final class VetSpecialtyEditModelTest {
 
 			// Test update
 			List<Entity> specialties = connection.select(VetSpecialty.VET_FK.equalTo(linda));
-			model.editor().clear();
+			model.editor().values().clear();
 			model.editor().entity().set(specialties.get(0));
 			model.editor().value(VetSpecialty.SPECIALTY).set(specialties.get(1).get(VetSpecialty.SPECIALTY));
 			assertThrows(EntityValidationException.class, model::update);
