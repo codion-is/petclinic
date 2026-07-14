e44333f56e32bc9da77f45eed84dbc71edac9615
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-07-14 09:37:43 +0000

    Redundant EntityPanel caption() config removed

--- src/main/java/is/codion/demos/petclinic/ui/PetclinicAppPanel.java
+++ src/main/java/is/codion/demos/petclinic/ui/PetclinicAppPanel.java
@@ -86,8 +86,7 @@ public final class PetclinicAppPanel extends EntityApplicationPanel<PetclinicApp
 		petTypeModel.tableModel().items().refresh();
 
 		return new EntityPanel(petTypeModel,
-						new PetTypeEditPanel(petTypeModel.editModel()), config ->
-						config.caption("Pet types"));
+						new PetTypeEditPanel(petTypeModel.editModel()));
 	}
 
 	private static EntityPanel createSpecialtyPanel(EntityConnectionProvider connectionProvider) {
@@ -96,8 +95,7 @@ public final class PetclinicAppPanel extends EntityApplicationPanel<PetclinicApp
 		specialtyModel.tableModel().items().refresh();
 
 		return new EntityPanel(specialtyModel,
-						new SpecialtyEditPanel(specialtyModel.editModel()), config -> config
-						.caption("Specialties"));
+						new SpecialtyEditPanel(specialtyModel.editModel()));
 	}
 
 	private static EntityPanel createVetPanel(EntityConnectionProvider connectionProvider) {
@@ -109,8 +107,7 @@ public final class PetclinicAppPanel extends EntityApplicationPanel<PetclinicApp
 		vetModel.tableModel().items().refresh();
 
 		EntityPanel vetPanel = new EntityPanel(vetModel,
-						new VetEditPanel(vetModel.editModel()), config -> config
-						.caption("Vets"));
+						new VetEditPanel(vetModel.editModel()));
 		EntityPanel vetSpecialtyPanel = new EntityPanel(vetSpecialtyModel,
 						new VetSpecialtyEditPanel(vetSpecialtyModel.editModel()), config -> config
 						.caption("Specialty"));
